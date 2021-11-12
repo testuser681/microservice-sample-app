9cf0eda932c9106cd96bfa7cdff50f9cd4d91524
testuser681 <84182009+testuser681@users.noreply.github.com>
2021-11-12 10:16:29 +0100

    add port for postgres

--- worker/src/main/java/worker/Worker.java
+++ worker/src/main/java/worker/Worker.java
@@ -68,7 +68,7 @@ class Worker {
     try {
 
       Class.forName("org.postgresql.Driver");
-      String url = "jdbc:postgresql://" + host + "/postgres";
+      String url = "jdbc:postgresql://" + host + ":5432/postgres";
 
       while (conn == null) {
         try {
