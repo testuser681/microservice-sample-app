7167cedcf494ca11a800a316c388b1b72d520c5a
testuser681 <84182009+testuser681@users.noreply.github.com>
2021-11-15 12:27:54 +0100

    update db address in server

--- result/server.js
+++ result/server.js
@@ -24,7 +24,7 @@ io.sockets.on('connection', function (socket) {
 });
 
 var pool = new pg.Pool({
-  connectionString: 'postgres://postgres:postgres@db/postgres'
+  connectionString: 'postgres://postgres:postgres@postgresql.jenkins.svc.cluster.local:5432/postgres'
 });
 
 async.retry(
